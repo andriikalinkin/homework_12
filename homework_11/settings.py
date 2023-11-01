d8179349880c0f96052992b600dc93a29efdb66c
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-11-02 00:22:41 +0100

    Add `exchange` app to settings.py

--- homework_11/settings.py
+++ homework_11/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "exchange.apps.ExchangeConfig",
     "django_celery_beat",
 ]
 
