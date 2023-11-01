a427cd1f914f33808110b6a8e31edda4eaab424c
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-11-02 00:20:23 +0100

    Models

--- exchange/models.py
+++ exchange/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Rate(models.Model):
+    currency_from = models.CharField(max_length=3)
+    currency_to = models.CharField(max_length=3)
+    rate = models.DecimalField(decimal_places=4, max_digits=10)
+    date = models.DateField()
