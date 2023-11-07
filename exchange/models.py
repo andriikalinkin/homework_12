28967c87daac0e73dd5a305e258cc4d48bd1180b
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-11-07 19:24:34 +0100

    Add `vendor` column to exchange_rate DB.

--- exchange/models.py
+++ exchange/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Rate(models.Model):
-    currency_from = models.CharField(max_length=3)
-    currency_to = models.CharField(max_length=3)
+    vendor = models.CharField(max_length=55)
+    currency_a = models.CharField(max_length=3)
+    currency_b = models.CharField(max_length=3)
     buy = models.DecimalField(decimal_places=4, max_digits=10, null=True)
     sell = models.DecimalField(decimal_places=4, max_digits=10, null=True)
     date = models.DateField()
